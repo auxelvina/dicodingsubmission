754b3d0dde99f1094ae932e733b37671926c1b1a
auxelvina <aux.elvina@gmail.com>
2020-01-19 21:59:58 +0700

    Update index.php

--- index.php
+++ index.php
@@ -26,10 +26,10 @@
        <input type="submit" name="load_data" value="Load Data" />
  </form>
  <?php
-    $host = "<Nama server database Anda>";
-    $user = "<Nama admin database Anda>";
-    $pass = "<Password admin database Anda>";
-    $db = "<Nama database Anda>";
+    $host = "svrdbelvina.database.windows.net";
+    $user = "elvina";
+    $pass = "Qwerty123";
+    $db = "dbelvinasubmission";
 
     try {
         $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
